0143fcb1d657070a9125d8f33d628df9645504dc
thramania <thramania@hanmail.net>
2025-11-20 14:13:09 +0900

    test added

--- src/main/java/com/dev/IbioScience/controller/customerPage/ProductFrontController.java
+++ src/main/java/com/dev/IbioScience/controller/customerPage/ProductFrontController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dev.IbioScience.dto.page.productList.ProductSortOption;
 
@@ -15,6 +16,13 @@ import com.dev.IbioScience.dto.page.productList.ProductSortOption;
 public class ProductFrontController {
 
 
+	@GetMapping("/testControllerMapping")
+	@ResponseBody
+	public String testControllerMapping() {
+		
+		return "success";
+	}
+	
 	 /**
      * 제품 리스트 페이지 진입
      *
